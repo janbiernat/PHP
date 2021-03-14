71c2de10f89d3c7709767b2139e02b236aff10b7
Jan T. Biernat <46463762+janbiernat@users.noreply.github.com>
2021-03-14 20:17:10 +0100

    Add files via upload

---
+++ select(combobox).php
@@ -0,0 +1,54 @@
+﻿<HTML><HEAD><META CHARSET="utf-8" /> 
+<TITLE>Select ala ComboBox (c)by Jan T. Biernat</TITLE> 
+</HEAD> 
+<?php 
+  function SelectComboBox($Tablica, $Sortuj = false, $Nazwa = "") { 
+  //SelectComboBox - Generowanie rozwijanej listy wyboru ala ComboBox. 
+    if($Sortuj == true) { Sort($Tablica); } 
+    if(trim($Nazwa) != "") { echo "<SELECT ".$Nazwa.">\n"; } 
+    echo "<OPTION>&nbsp;</OPTION>"; 
+    for($I = 0; $I < count($Tablica); $I++) { 
+      echo "\n<OPTION>".$Tablica[$I]."</OPTION>"; 
+    } 
+    if(trim($Nazwa) != "") { echo "\n</SELECT>\n"; } 
+  } 
+?> 
+<BODY> 
+<!-- Wywołanie funkcji "SelectComboBox". --> 
+Komputery:&nbsp;<SELECT NAME="ComboBox"> 
+<?php 
+  //Wywołanie funkcji "SelectComboBox" - metoda 1. 
+    $Dane = array( 
+                   "Atari", 
+                   "Commodore", 
+                   "ZX Spectrum", 
+                   "Amstrad", 
+                   "Cry", 
+                   "IBM", 
+                   "Amiga", 
+                   "Atari ST", 
+                   "Atari TT", 
+                   "Atari Portfolio" 
+                  ); 
+    SelectComboBox($Dane); 
+?> 
+</SELECT> 
+<BR /><BR /> 
+Komputery: 
+<?php 
+  //Wywołanie funkcji "SelectComboBox" - metoda 2. 
+    echo "\n\n"; 
+    $Dane2[] = "Atari"; 
+    $Dane2[] = "Commodore"; 
+    $Dane2[] = "ZX Spectrum"; 
+    $Dane2[] = "Amstrad"; 
+    $Dane2[] = "Cry"; 
+    $Dane2[] = "IBM"; 
+    $Dane2[] = "Amiga"; 
+    $Dane2[] = "Atari ST"; 
+    $Dane2[] = "Atari TT"; 
+    $Dane2[] = "Atari Portfolio"; 
+    SelectComboBox($Dane2, true, "NAME=\"Lista\" STYLE=\"width:150px;\""); 
+?> 
+</BODY> 
+</HTML> 
